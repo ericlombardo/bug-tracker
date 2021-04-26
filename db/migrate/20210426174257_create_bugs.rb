eb38c63b5daca308130d2310f0d433c886fd115a
Eric <eric.m.lombardo@gmail.com>
2021-04-26 13:45:23 -0400

    Create resources for Bug
        * Migration to create table with starting attributes
        * Bug
          * Model
          * Controller
          * Views folder
          * Helper file
          * Assets
          * Resources :bugs in routes.rb file

---
+++ db/migrate/20210426174257_create_bugs.rb
@@ -0,0 +1,18 @@
+class CreateBugs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bugs do |t|
+      t.string :type
+      t.string :priority
+      t.string :status
+      t.string :overview
+      t.text :description
+      t.date :created
+      t.text :notes
+      t.text :next_steps
+      t.string :update_for_client
+      t.string :program_id
+
+      t.timestamps
+    end
+  end
+end
