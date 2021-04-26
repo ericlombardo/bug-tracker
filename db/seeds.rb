dfbfe51ecb19227c28c6f2fd41510c467e29d998
Eric <eric.m.lombardo@gmail.com>
2021-04-26 15:38:49 -0400

    Create user, bug, and program for initial testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 
 # create bugs
 # Bug.create(category: "security", priority: "critical", status: "pending", overview: "passwords being shown to users not logged in", description: "Users are allowed to see a master list of the users with passwords included", created: Date.current, notes: "Had this issue before, was not fixed properly", next_steps: "Try to reset change the session[:user_id] upon login to restrict the access given to the user", update_for_client: "Working through updates needed. Important to change your password for the time being.")
+
+# create a user
+user = User.create(name: "bill", email: "bill@gmail.com", role: "dev", password: "asdfasdf")
+# create a program and link to user
+user.programs.create(name: "MailMe", status: "Active", description: "Program to help people connect through email who might not have met otherwise", opened_date: Date.current)
+# create a bug and link to program
+user.programs.first.bugs.create(category: "security", priority: "critical", status: "pending", overview: "passwords being shown to users not logged in", description: "Users are allowed to see a master list of the users with passwords included", created: Date.current, notes: "Had this issue before, was not fixed properly", next_steps: "Try to reset change the session[:user_id] upon login to restrict the access given to the user", update_for_client: "Working through updates needed. Important to change your password for the time being.")
+# test all associations
+  # user.programs
+  # user.bugs
+  # program.users
+  # program.bugs
+  # bug.program
+  # bug.users
