56738df10c55406d805af121b24b312c7fb94b4e
Eric <eric.m.lombardo@gmail.com>
2021-04-26 14:19:05 -0400

    Test creation of each model for functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create users
+# User.create(name: "bill", email: "bill@gmail.com", role: "dev", password: "asdfasdf")
+
+# create programs
+# Program.create(name: "MailMe", status: "Active", description: "Program to help people connect through email who might not have met otherwise", opened_date: Date.current)
+
+# create bugs
+# Bug.create(category: "security", priority: "critical", status: "pending", overview: "passwords being shown to users not logged in", description: "Users are allowed to see a master list of the users with passwords included", created: Date.current, notes: "Had this issue before, was not fixed properly", next_steps: "Try to reset change the session[:user_id] upon login to restrict the access given to the user", update_for_client: "Working through updates needed. Important to change your password for the time being.")
