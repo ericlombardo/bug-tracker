d36375bde954b3075000ceed124f1da94070e938
Eric <eric.m.lombardo@gmail.com>
2021-04-26 16:39:01 -0400

    Build seed file for database

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+# Gem to create fake data for database testing
+gem 'faker', '~> 1.6', '>= 1.6.6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,12 @@
-# create users
-# User.create(name: "bill", email: "bill@gmail.com", role: "dev", password: "asdfasdf")
-
-# create programs
-# Program.create(name: "MailMe", status: "Active", description: "Program to help people connect through email who might not have met otherwise", opened_date: Date.current)
-
-# create bugs
-# Bug.create(category: "security", priority: "critical", status: "pending", overview: "passwords being shown to users not logged in", description: "Users are allowed to see a master list of the users with passwords included", created: Date.current, notes: "Had this issue before, was not fixed properly", next_steps: "Try to reset change the session[:user_id] upon login to restrict the access given to the user", update_for_client: "Working through updates needed. Important to change your password for the time being.")
-
-# create a user
-user = User.create(name: "bill", email: "bill@gmail.com", role: "dev", password: "asdfasdf")
-# create a program and link to user
-user.programs.create(name: "MailMe", status: "Active", description: "Program to help people connect through email who might not have met otherwise", opened_date: Date.current)
-# create a bug and link to program
-user.programs.first.bugs.create(category: "security", priority: "critical", status: "pending", overview: "passwords being shown to users not logged in", description: "Users are allowed to see a master list of the users with passwords included", created: Date.current, notes: "Had this issue before, was not fixed properly", next_steps: "Try to reset change the session[:user_id] upon login to restrict the access given to the user", update_for_client: "Working through updates needed. Important to change your password for the time being.")
-# test all associations
-  # user.programs
-  # user.bugs
-  # program.users
-  # program.bugs
-  # bug.program
-  # bug.users
+# create 5 projects and link 3 new users and 5 new bugs to each
+# 5.times do  
+#   prog = Program.create(name: Faker::Company.name, status: Faker::Lorem.words, description: Faker::Lorem.paragraph(2), opened_date: Faker::Date.backward)
+#   users = []
+#   3.times {users << User.create(name: Faker::Name.name, email: Faker::Internet.email, role: "dev", password: Faker::Internet.password(8))}
+  
+#   bugs = []
+#   5.times {bugs << Bug.create(category: Faker::Lorem.word, priority: Faker::Lorem.word, status: Faker::Lorem.word, overview: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, created: Faker::Date.backward, notes: Faker::Lorem.paragraph(1), next_steps: Faker::Lorem.paragraph(1), update_for_client: Faker::Lorem.sentence(1) )}
+  
+#   prog.users << users  
+#   prog.bugs << bugs
+# end
