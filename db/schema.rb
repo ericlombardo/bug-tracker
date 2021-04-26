34b60bd60546ca70dc467c06854a83408d9ce515
Eric <eric.m.lombardo@gmail.com>
2021-04-26 13:58:10 -0400

    Run migrations for database

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_26_175355) do
+
+  create_table "bugs", force: :cascade do |t|
+    t.string "type"
+    t.string "priority"
+    t.string "status"
+    t.string "overview"
+    t.text "description"
+    t.date "created"
+    t.text "notes"
+    t.text "next_steps"
+    t.string "update_for_client"
+    t.string "program_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "programs", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.text "description"
+    t.date "opened_date"
+    t.date "closed_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "programs_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "program_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "role"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
