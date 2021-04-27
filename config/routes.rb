66ca1115384d7732e887940346c2e00727a3c7d4
Eric <eric.m.lombardo@gmail.com>
2021-04-26 20:14:30 -0400

    Build route/action/view for /users/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
   resources :bugs
   resources :programs
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
