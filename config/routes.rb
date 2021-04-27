e262445b3e7127186b200dbb5a9ffc05a4ca1b07
Eric <eric.m.lombardo@gmail.com>
2021-04-27 13:43:44 -0400

    Refactor user routes to what is currently needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create]
   resources :bugs
   resources :programs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
