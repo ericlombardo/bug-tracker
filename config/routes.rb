35dee8658bc9df5e547d696dab1e020c7feb18af
Eric <eric.m.lombardo@gmail.com>
2021-04-27 14:32:56 -0400

    Add logout route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#logout"
   resources :users, only: [:new, :create]
   resources :bugs
   resources :programs
