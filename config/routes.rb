de48432d6416384ea4850c6c36f79535f88fdd39
Eric <eric.m.lombardo@gmail.com>
2021-04-27 14:46:29 -0400

    Adjust routes from "/users/new" to "/signup"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#logout"
-  resources :users, only: [:new, :create]
+  post '/logout', to: "sessions#destroy"
+  resources :users
   resources :bugs
   resources :programs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
