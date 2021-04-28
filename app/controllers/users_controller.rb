98bb1a14f08fe57196e13816d8cf9dd1de52a1b5
Eric <eric.m.lombardo@gmail.com>
2021-04-28 13:03:58 -0400

    Remove logic to seperate concerns in client_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,11 @@ class UsersController < ApplicationController
     @user = User.new(role: "client")
   end
 
-  def create
+  def client_create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id 
-      if @user.role == "client"
-        redirect_to programs_path
-      end
+      redirect_to programs_path
     else
       render :new
     end
