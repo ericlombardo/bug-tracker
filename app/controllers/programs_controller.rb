d1be100a5ff1ce5ce5a9ab1676bf4d377fe088ed
Eric <eric.m.lombardo@gmail.com>
2021-04-28 15:47:33 -0400

    Create logic for new program form in  action

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -7,4 +7,10 @@ class ProgramsController < ApplicationController
   def show
     @program = Program.find_by(id: params[:id])
   end
+
+  def new
+    @program = Program.new
+    @devs = Program.get_devs # collect all users where role == "dev"
+    @prj_manager = Program.get_prj_managers # collect all users where role == "prj_manager"
+  end
 end
