8d8529b4989dbe67f99a332033eb7c27c12070be
Eric <eric.m.lombardo@gmail.com>
2021-04-28 13:46:22 -0400

    remove '_' from routes for restful conventions

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   root "sessions#welcome" # user selects if client or employee
 
   # routes for client login and signup
-  get '/client_login', to: "sessions#client_new"  
-  post '/client_login', to: "sessions#client_create"
-  get '/client_signup', to: "users#client_new"
-  post'/client_signup', to: "users#client_create"
+  get '/client/login', to: "sessions#client_new"  
+  post '/client/login', to: "sessions#client_create"
+  get '/client/signup', to: "users#client_new"
+  post'/client/signup', to: "users#client_create"
   
   # routes for employee login and signup
-  get '/employee_login', to: "sessions#employee_new"
-  post '/employee_login', to: "sessions#employee_create"
-  get '/employee_signup', to: "users#employee_new"
-  post '/employee_signup', to: "users#employee_create"
+  get '/employee/login', to: "sessions#employee_new"
+  post '/employee/login', to: "sessions#employee_create"
+  get '/employee/signup', to: "users#employee_new"
+  post '/employee/signup', to: "users#employee_create"
 
   post '/logout', to: "sessions#destroy"
   resources :users
