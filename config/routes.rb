cf7c328e4e5964d5978ce1596f4980762e5fcb45
Eric <eric.m.lombardo@gmail.com>
2021-04-28 13:19:34 -0400

    Add routes for employee signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root "sessions#welcome"
+  root "sessions#welcome" # user selects if client or employee
 
-  get '/client_login', to: "sessions#client_new"
+  # routes for client login and signup
+  get '/client_login', to: "sessions#client_new"  
   post '/client_login', to: "sessions#client_create"
   get '/client_signup', to: "users#client_new"
   post'/client_signup', to: "users#client_create"
   
+  # routes for employee login and signup
+  get '/employee_login', to: "sessions#employee_new"
+  post '/employee_login', to: "sessions#employee_create"
+  get '/employee_signup', to: "users#employee_new"
+  post '/employee_signup', to: "users#employee_create"
 
   post '/logout', to: "sessions#destroy"
   resources :users
