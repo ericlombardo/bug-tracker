3745573f8ffed7bafe73adfdd65bcddbbeed1f23
Eric <eric.m.lombardo@gmail.com>
2021-04-28 12:34:07 -0400

    Fix redirect for new client signup in def create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id 
-      redirect_to programs_path
+      if @user.role == "client"
+        redirect_to programs_path
+      end
     else
       render :new
     end
