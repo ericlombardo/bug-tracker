e1b6f3258b43a4ba128cc70fad9f4d056ce23f88
Eric <eric.m.lombardo@gmail.com>
2021-04-28 12:47:45 -0400

    Fix action names  for client_new and client_create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,23 @@ class SessionsController < ApplicationController
   def welcome
   end
 
-  def new # show new login form
+  def client_new # show new login form
   end
 
-  def create
+  def client_create
     user = User.find_by(email: params[:email])  # find the user in database using email
     if user && user.authenticate(params[:password]) # make sure user exists and password matches
       session[:user_id] = user.id # assign user id to session hash
       redirect_to programs_path # redirect desired view
     else  # if user not found or wrong password
       flash.alert = "No user found. Try again or Signup"  # put message in flash hash
-      redirect_to login_path  # redirect to login view to show alert
+      redirect_to client_login_path  # redirect to login view to show alert
     end
   end
 
   def destroy # check for right user, redirect of not
     session.delete :user_id # removes user_id from session hash
-    redirect_to login_path  # redirects to login page
+    redirect_to root_path  # redirects to login page
   end
  
   private
