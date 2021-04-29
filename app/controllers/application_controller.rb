3e37184ca7a51e460d6f5304a262a750f941d62b
Eric <eric.m.lombardo@gmail.com>
2021-04-29 19:45:59 -0400

    Move helper methods to ApplicationHelper folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
-  private
-
-  def current_user  # return user instance if logged in and present
-    User.find_by(id: session[:user_id])
-  end
-
-  def logged_in? # returns user_id if present => redirects to root if not
-    session[:user_id] ? (session[:user_id]) : (redirect_to root_path)
-  end
+  include ApplicationHelper
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_user  # return user instance if logged in and present
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in? # returns user_id if present => redirects to root if not
+    session[:user_id] ? (session[:user_id]) : (redirect_to root_path)
+  end
 end
