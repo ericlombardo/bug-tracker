e05ff88b2a00eae452ecd152a49a2943bc16a6dc
Eric <eric.m.lombardo@gmail.com>
2021-04-29 14:10:11 -0400

    Build #current_user and #logged_in? methods
    Methods built in ApplicationController so other controllers have access
    to them throughout the program

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def current_user  # return user instance if logged in and present
+    User.find_by(id: logged_in?)
+  end
+
+  def logged_in? # returns user_id if present => redirects to root if not
+    session[:user_id] ? (session[:user_id]) : (redirect_to root_path)
+  end
 end
