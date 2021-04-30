5fda5ce3f1f9b96aab299cbc2adac1823dc8b618
Eric <eric.m.lombardo@gmail.com>
2021-04-30 13:36:06 -0400

    Add helper methods in ProgramsHelper and include

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-
+  include ProgramsHelper
 end
--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,2 +1,10 @@
 module ProgramsHelper
+
+  def opened_date
+    @program.opened_date.strftime("%A %d, %Y")
+  end
+
+  def closed_date
+    @program.closed_date.strftime("%A %d, %Y") if @program.closed_date
+  end
 end
