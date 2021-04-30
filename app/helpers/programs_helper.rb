6852d64485d97175e1c7fff6135fc7986d69f846
Eric <eric.m.lombardo@gmail.com>
2021-04-30 18:11:29 -0400

    Fix closed_date checkbox in programs#edit view

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -3,6 +3,10 @@ module ProgramsHelper
   def check?(id)
     @program.users.any? {|u| u.id == id}
   end
+
+  def closed?
+    @program.closed_date.instance_of?(Date)
+  end
   def opened_date
     @program.opened_date.strftime("%A %d, %Y")
   end
