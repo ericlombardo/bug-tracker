bba44e146b1f7415bf3c15f71f5050e4b7ab86e3
Eric <eric.m.lombardo@gmail.com>
2021-05-03 11:04:11 -0400

    Build basic users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+  end
   def edit
     @user = User.find_by(id: params[:id])
   end
