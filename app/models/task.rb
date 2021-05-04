995874b13408f27673862e60ccfb2fa6edc71c6b
Eric <eric.m.lombardo@gmail.com>
2021-05-04 14:26:44 -0400

    Add validations for the Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
     # associations
     belongs_to :user
-    belongs_to :program
+    belongs_to :program, optional: true
+
+    # validations
+    validates :urgency, presence: true
+    validates :title, presence: true, length: { maximum: 50}
+    validates :program_id, presence: {message: "must be selected to continue"}
 end
