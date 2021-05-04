cd001894add2a456224714f91d5f87a26fe0d0e8
Eric <eric.m.lombardo@gmail.com>
2021-05-04 11:43:10 -0400

    Add functionality to upload program image
        * Install active_storage
        * Set test and dev. storage to local disk
        * Set environment files to proper location
        * Add association in Program model
        * Add upload field in new/edit view of program

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,11 +1,13 @@
 class Program < ApplicationRecord
   include ScopeMethods  # pulls in scope methods from app/models/concerns
   
+
   # associations
   has_many :programs_users
   has_many :users, through: :programs_users
   has_many :bugs
   has_many :tasks
+  has_one_attached :app_logo
 
   def bug_count(status)
     self.bugs.where("status == ?", status).count 
