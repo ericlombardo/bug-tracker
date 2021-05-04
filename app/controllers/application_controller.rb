4d6eecdd2e405609434281807866dc0584079244
Eric <eric.m.lombardo@gmail.com>
2021-05-04 16:51:33 -0400

    Write authenticate method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   include ProgramsHelper
+
+  def authenticate
+    unless logged_in?
+      redirect_to root_path
+    end
+  end
 end
