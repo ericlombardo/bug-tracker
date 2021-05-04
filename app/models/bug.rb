2a8c966a8eb0eea26594f8e4090d7f66304a1ab9
Eric <eric.m.lombardo@gmail.com>
2021-05-04 14:41:34 -0400

    Add validations to Bug model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,5 +1,9 @@
 class Bug < ApplicationRecord
   # associations
-  belongs_to :program
+  belongs_to :program, optional: true
   has_many :users, through: :program 
+
+  # validations
+  validates :category, :status, :overview, :description, presence: true
+  validates :program_id, presence: {message: "must be selected before creating a bug report"}
 end
