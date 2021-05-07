2403bda8b9f325993db3bad05708ea0aed6f5c98
Eric <eric.m.lombardo@gmail.com>
2021-05-07 18:39:32 -0400

    Refactor navbar to display based on role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def admin?
-    current_user.role == "admin"
+    current_user.role == "Admin"
   end
   
   def current_user  # return user instance if logged in and present
@@ -10,5 +10,18 @@ module ApplicationHelper
 
   def logged_in? # returns user_id if present => redirects to root if not
     id ||= session[:user_id] ? (session[:user_id]) : (redirect_to root_path)
+  end
+
+  def display_navbar
+    case current_user.role
+    when "Client"
+      render "layouts/client_nav"
+    when "Developer"
+      render "layouts/employee_nav"
+    when "Project Manager"
+      render "layouts/employee_nav"
+    when "Admin"
+      render "layouts/admin_nav"
+    end
   end
 end
