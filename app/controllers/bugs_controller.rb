aa403b5183504d34f2fdd5dc888377354eaa8faa
Eric <eric.m.lombardo@gmail.com>
2021-05-07 14:03:48 -0400

    Finish refactor of Bugs model
    * Adjust form_with to exclude defining @program in controller
    * Create helpers for index action
        * assign_program_and_bugs
        * assign_user_and_bugs
        * assign_and_redirect_user
    * Commented everything out

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,39 +1,25 @@
 class BugsController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate # confirms user is logged in
   
   def index
-    if params[:program_id] # if nested_program? #=> instance of program 
-      @program = Program.find_by(id: params[:program_id])
-      @bugs = @program.bugs 
-    elsif params[:user_id]  # elsif nested_user? #=> instance of user
-      if params[:user_id].to_i == current_user.id
-        @user = User.find_by(id: params[:user_id])
-        @bugs = @user.bugs
-      else
-        redirect_to user_bugs_path(current_user), info: "These are your bugs"
-      end
-    else
-      redirect_to programs_path, info: "Please select a program in order to view bugs"
+    if params[:program_id] # if 'program bugs'
+      assign_program_and_bugs #apab(.) Sets @program and @bugs
+    elsif params[:user_id]  # elsif 'user bugs'
+      assign_and_redirect_user #aaru(.) Sets @user and @bugs or redirects to correct user index
     end
   end
 
   def new
-    if params[:program_id] && @prog = Program.find_by(id: params[:program_id])
-      @bug = @prog.bugs.build(status: "pending")
+    if params[:program_id] && @program = Program.find_by(id: params[:program_id])
+      @bug = @program.bugs.build(status: "pending")
     else
-      # message about selecting a program before reporting a bug
-      redirect_to programs_path
+      redirect_to programs_path, alert: "You must select a program before reporting a bug"
     end
   end
 
   def create
-    @prog = Program.find_by(id: params[:program_id])
     @bug = Bug.new(bug_params)  # create new bug instance
-    if @bug.save  # make sure it is valid/saves
-      redirect_to [@bug.program, @bug]  
-    else
-      render :new
-    end
+    @bug.save ? (redirect_to [@bug.program, @bug]) : (render :new)  
   end
 
   def edit
@@ -42,20 +28,37 @@ class BugsController < ApplicationController
 
   def update 
     @bug = Bug.find_by(id: params[:id])
-    if @bug.update(bug_params)
-      redirect_to program_bug_path(@bug)
-    else
-      render :edit # show error messages
-    end
+    @bug.update(bug_params) ? (redirect_to program_bug_path(@bug)) : (render :edit)
   end
   
   def show
     @bug = Bug.find_by(id: params[:id])
   end
 
-  private
+  private ################################################################
 
   def bug_params
     params.require(:bug).permit(:category, :priority, :status, :overview, :description, :created, :notes, :next_steps, :update_for_client, :program_id)
+  end
+
+  def assign_program_and_bugs # assign @bugs and @program if program found, redirect if not
+    if @program = Program.find_by(id: params[:program_id])
+      @bugs = @program.bugs
+    else
+      redirect_to programs_path, alert: "Info for the program not found"
+    end
+  end
+
+  def assign_user_and_bugs
+    @user = User.find_by(id: params[:user_id])
+    @bugs = @user.bugs
+  end
+
+  def assign_and_redirect_user # sets @users and @bugs if correct user, redirects if not
+    if params[:user_id].to_i == current_user.id #
+      assign_user_and_bugs #auab(.) sets @user and @bugs
+    else
+      redirect_to user_bugs_path(current_user), alert: "These are your bugs"
+    end
   end
 end
